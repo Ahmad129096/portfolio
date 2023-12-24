a470e985fdd36aee6c792ed868a4b26c02cb8ff1
Ahmad Hassan <ahmad.hassan.dev@pk.see.biz>
2023-12-24 11:27:14 +0500

    feat: implemented contact section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,66 @@
+"use client";
+import Circles from "@/components/Circles";
+import { BsArrowRight } from "react-icons/bs";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+      <Circles />
+      <div
+        className="container mx-auto h-full flex flex-col items-center
+    xl:flex-row gap-x-6 mt-5"
+      >
+        <div className="flex-1 flex flex-col w-full max-w-[700px]">
+          <motion.h2
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2 text-center mb-12"
+          >
+            Let&apos;s <span className="text-accent">connect.</span>
+          </motion.h2>
+          <motion.form
+            variants={fadeIn("up", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            action=""
+            className="flex-1 flex flex-col gap-6 w-full mx-auto"
+          >
+            <div className="flex gap-x-6 w-full">
+              <input type="text" placeholder="name" className="input" />
+              <input type="text" placeholder="email" className="input" />
+            </div>
+            <input type="text" placeholder="subject" className="input" />
+            <textarea
+              name="message"
+              placeholder="message"
+              id=""
+              className="textarea"
+            ></textarea>
+            <button
+              className="btn rounded-full border border-white/50
+              max-w-[170px] px-8 transition-all duration-300 flex items-center
+              justify-center overflow-hidden hover:border-accent group"
+            >
+              <span
+                className="group-hover:translate-y-[120%] group-hover:opacity-0
+                transition-all duration-500"
+              >
+                lets talk
+              </span>
+              <BsArrowRight
+                className="-translate-y-[120%] opacity-0 group-hover:flex
+                group-hover:-translate-y-0 group-hover:opacity-100 transition-all
+                duration-100 absolute text-[22px]"
+              />
+            </button>
+          </motion.form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,11 +7,17 @@ const Header = () => {
     <header className="absolute z-30 w-full items-center flex px-16 xl:px-0 xl:h-[90px]">
       <div className="container mx-auto">
         <div
-          className="flex flex-col lg:flex-row justify-between items-center gap-y-6
-      py-8"
+          className="flex flex-col lg:flex-row justify-between items-center gap-y-0 xl:gap-y-6
+      xl:py-8"
         >
           <Link href="/">
-            <Image src="/logo.png" alt="" width={200} height={200} />
+            <Image
+              src="/logo.png"
+              alt=""
+              width={100}
+              height={100}
+              className="object-none"
+            />
           </Link>
           <Socials />
         </div>
