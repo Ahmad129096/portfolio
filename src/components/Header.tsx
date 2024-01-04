ad111e036c16bfe6c7c93855c7ab9ea8d7d11b8d
Ahmad Hassan <ahmad.hassan.dev@pk.see.biz>
2024-01-04 11:48:12 +0500

    changed logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,17 @@
 import Link from "next/link";
 import Socials from "./Socials";
-import Image from "next/image";
 
 const Header = () => {
   return (
     <header className="absolute z-30 w-full items-center flex px-16 xl:px-0 xl:h-[90px]">
       <div className="container mx-auto">
         <div
-          className="flex flex-col lg:flex-row justify-between items-center gap-y-0 xl:gap-y-6
-      xl:py-8"
+          className="flex flex-col lg:flex-row justify-between items-center gap-y-3 xl:gap-y-6
+      xl:py-8 mt-10"
         >
-          <Link href="/">
-            <Image
-              src="/logo.png"
-              alt=""
-              width={100}
-              height={100}
-              className="object-none"
-            />
+          <Link href="/" className="font-bold text-lg xl:text-[30px]">
+            <span className="text-accent">Elevate</span>{" "}
+            <span className="bg-accent p-2 rounded-md"> Tech</span>
           </Link>
           <Socials />
         </div>
